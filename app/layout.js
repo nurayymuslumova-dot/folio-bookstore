a9e67b0ccf0a951c75357ebccddaa5663833d782
Nuray <nurayymuslumova@gmail.com>
2026-07-01 03:40:03 +0400

    add google analytics

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import './globals.css';
 import { Plus_Jakarta_Sans } from 'next/font/google';
+import Script from 'next/script';
 
 const plusJakarta = Plus_Jakarta_Sans({
   subsets: ['latin'],
@@ -15,6 +16,20 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-0CWG7W7LW1"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-0CWG7W7LW1');
+          `}
+        </Script>
+      </head>
       <body className={`${plusJakarta.className} bg-black text-white antialiased`}>{children}</body>
     </html>
   );
