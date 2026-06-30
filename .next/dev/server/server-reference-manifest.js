2ab530f7bea768a7138d29b0dc9b458c6ab26385
Nuray <nurayymuslumova@gmail.com>
2026-06-30 15:49:28 +0400

    chore: .next ve node_modules silindi, temiz repo

--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"xoXT02Oocp+Vtnz4a9DOFwVTZ5JBLrrcPaxuyhrllAE=\"\n}"
--- .next/dev/static/chunks/app_page_0-lapbc.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/app_page_1thsms_.js","static/chunks/node_modules_1uk-o7a._.js"],
-    source: "dynamic"
-});
